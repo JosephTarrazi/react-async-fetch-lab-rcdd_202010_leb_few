3aeb8d13896b105a37eabf47ca2c8d6378c32eae
Joseph Tarrazi <joseftaerrazi@gmail.com>
2021-01-19 10:57:25 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react'
 
 class App extends Component {
@@ -24,3 +25,33 @@ class App extends Component {
 }
 
 export default App
+=======
+// create your App component here
+// create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+      peopleInSpace: []
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(({people}) => this.setState({ peopleInSpace: people }))
+
+  }
+
+}
+
+export default 
+>>>>>>> fd590457ca993fd9b28b12486b0356eef05ef3e3
